4a03d455825dd979c0313bd47c7aec71f05c904f
rambhardwajj <rambhardwaj101000@gmail.com>
2025-02-14 10:39:11 +0530

    myIncludes.js

---
+++ Pollyfills/myIncludes.js
@@ -0,0 +1,22 @@
+
+if( !Array.prototype.myIncludes){
+    Array.prototype.myIncludes = function(searchElement , startIndex = 0){
+        startIndex = startIndex>=0 ? startIndex: Math.max( 0, this.length-Math.abs(startIndex));
+
+        for( let i = startIndex; i< this.length; i++){
+            if( this[i] == searchElement || ( Number.isNaN(this[i]) && Number.isNaN(searchElement)) ){
+                return true;
+            }
+            // Or 
+            // if (Object.is(this[i], searchElement)) {
+            //     return true;
+            // }
+        }
+        return false;
+    }
+}
+
+console.log([1, 2, 3].myIncludes(2)); // true
+console.log([1, 2, 3].myIncludes(4)); // false
+console.log([NaN, 2, 3].myIncludes(NaN)); // true
+console.log([1, 2, 3].myIncludes(2, -1)); // false
