bf5dfb016eb3c88815fb03098cbfaa7426d2777e
rambhardwajj <rambhardwaj101000@gmail.com>
2025-02-17 12:05:52 +0530

    cancelled functionality

--- DOM/Todo/index.js
+++ DOM/Todo/index.js
@@ -8,23 +8,34 @@ const inp = document.getElementById('inp')
 console.log(inp)
 
 const doneArea = document.getElementById('compItems')
-console.log(doneArea)
+const cancelledArea = document.getElementById('cancelled');
+
 
 addBtn.addEventListener('click', ()=>{
     const listEl = document.createElement('li');
+
     const xbtn = document.createElement('button');
     xbtn.innerText = 'X'
+    const cancelBtn = document.createElement('button')
+    cancelBtn.innerText = 'Cancel'
     const doneBtn = document.createElement('button');
-    doneBtn.innerText='✔️'
+    doneBtn.innerText='Done'
+
     listEl.innerText = inp.value 
+
     ulElemnt.appendChild(listEl)
+
     listEl.appendChild(xbtn)
+    listEl.appendChild(cancelBtn)
     listEl.appendChild(doneBtn)
+
     inp.value=''
 
+    
     xbtn.addEventListener('click', ()=>{
         listEl.remove();
     })
+
     doneBtn.addEventListener('click', ()=>{
         const compItem = document.createElement('li');
         listEl.removeChild(doneBtn)
@@ -32,6 +43,14 @@ addBtn.addEventListener('click', ()=>{
         compItem.innerText = listEl.innerText;
         compItem.style.color = 'green'
         doneArea.appendChild(compItem)
+        listEl.remove();
+    })
+
+    cancelBtn.addEventListener('click', ()=>{
+        const cancelItem = document.createElement('li');
+        cancelItem.innerText = listEl.innerText;
+        cancelItem.style.color = 'red'
+        cancelledArea.appendChild(cancelItem);
         listEl.remove();
     })
 })
