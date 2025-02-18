c67275b00903ed63b9e3debf8ef6930be627bc55
rambhardwajj <rambhardwaj101000@gmail.com>
2025-02-18 23:17:13 +0530

    prototype chalenges

---
+++ Prototypes/Challenges/ch10.js
@@ -0,0 +1,40 @@
+// You need to implement the Product constructor function and its prototype methods
+
+function Product(name, price, stock) {
+    // Initialize name, price, and stock properties
+    this.name= name; this.price = price; this.stock = stock; 
+}
+
+// Define applyDiscount method on Product's prototype
+Product.prototype.applyDiscount = function(percent){
+  let val = (percent/100)*this.price;
+  this.price =Math.round( this.price - val);
+  return this.price;
+}
+// Define purchase method on Product's prototype
+Product.prototype.purchase = function(quantity){
+  if( this.stock< quantity)return "Not enough stock";
+  this.stock-= quantity;
+  return this.stock;
+}
+// Please don't remove the code below
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (input) => {
+  const { product, action } = JSON.parse(input);
+  const storeItem = new Product(product.name, product.price, product.stock);
+
+  if (action.type === "discount") {
+    storeItem.applyDiscount(action.percent);
+  } else if (action.type === "purchase") {
+    process.stdout.write(JSON.stringify(storeItem.purchase(action.quantity)));
+    return;
+  }
+
+  process.stdout.write(JSON.stringify([storeItem.price, storeItem.stock]));
+});
