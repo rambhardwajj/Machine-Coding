9ed36fa9832a6103da2c5a291de9cfd7b795c981
rambhardwajj <rambhardwaj101000@gmail.com>
2025-02-19 18:16:42 +0530

    added the dom phase 1

--- DOM/Review Collector/index.js
+++ DOM/Review Collector/index.js
@@ -2,25 +2,38 @@ const nameDisplay = document.getElementById('username')
 const textArea = document.getElementById('area')
 const submitBtn = document.getElementById('submitBtn')
 
-const custReviewSection = document.getElementById('cust-reviews');
+const recentReview = document.getElementById('review-display');
 
-console.log(nameDisplay, textArea, submitBtn, custReviewSection)
+console.log(nameDisplay, textArea, submitBtn, recentReview)
 
 submitBtn.addEventListener('click', ()=>{
-    const userName = nameDisplay.value;
+    let userName = '~ ' +  nameDisplay.value; 
+    if( userName == '~ ') {
+        userName = userName + "Anonymous";
+    }
     const userReview = textArea.value;
+    if(userReview.length ==0){
+        window.alert('Review toh daalo bhaisab')
+        return;
+    }
 
-    const reviewCard = document.createElement('div')
+    const recReviewCard = document.createElement('div')
     const subStars = document.createElement('div')
-    const para = document.createElement('span');
+    const para = document.createElement('div');
     const nameSpan = document.createElement('span')
         
     para.innerText = userReview;
     nameSpan.innerText = userName;
 
-    reviewCard.appendChild(subStars);
-    reviewCard.appendChild(para); 
-    reviewCard.appendChild(nameSpan)
-    // console.log(reviewCard)
+    recReviewCard.appendChild(subStars);
+    recReviewCard.appendChild(para); 
+    recReviewCard.appendChild(nameSpan)
+    console.log(recReviewCard)
+
+    recReviewCard.classList.add('js-rec-review')
+    para.classList.add('js-para')
+    nameSpan.classList.add('js-name')
+
+    recentReview.appendChild(recReviewCard)
 
 })
