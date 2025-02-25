3db0fe67cdf0f0dbaae2266ba57617079bc3abb1
rambhardwajj <rambhardwaj101000@gmail.com>
2025-02-25 13:20:00 +0530

    added concepts

---
+++ ImpJsConcepts/concept1.js
@@ -0,0 +1,18 @@
+
+// checking the type of incoming data
+
+let num = "42a"
+let converTedNum = Number(num)
+// let converTedNum = +num;
+// let converTedNum = parseInt(num);
+
+console.log(converTedNum) // NaN
+console.log(typeof converTedNum) // number
+
+
+let str = 123
+let convertedString = String(str)
+
+// ____________________________________________________
+
+// differnece between ==  ===
---
+++ ImpJsConcepts/impModules.js
@@ -0,0 +1,13 @@
+// play with Js modules: math & Date 
+console.log(Math.random())
+console.log(Math.random() *10);
+
+
+
+let message = "sfdfd ssdfa  asf"
+console.log(message.toUpperCase())
+console.log(message.indexOf("d"))
+console.log(message.slice(0,5));
+
+let myName = "Ram";
+let greeting  = `Hello ${myName}, from Mathura `
